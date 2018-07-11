79ca429185192ea027db943f6ea0e98f57bcdf8d
RmbRT <steffen.rattay@gmail.com>
2018-07-11 21:39:44 +0200

    Fixed error in byte order exchange.

--- src/remote/SerialisedConnection.cpp
+++ src/remote/SerialisedConnection.cpp
@@ -27,7 +27,9 @@ namespace crystal::remote
 
 	void SerialisedConnection::exchange_byte_order()
 	{
-		*this << m_send >> m_receive;
+		*this << m_send;
+
+		deserialise(&m_receive, sizeof(util::Endian));
 
 		if(!eie::valid(m_receive))
 			throw std::runtime_error("Illegal endianness value received.");
